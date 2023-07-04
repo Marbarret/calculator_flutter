c83bb3e30562392eec31a6633f2ca05ec818f9c3
MarBarret <marsouza93@gmail.com>
2023-07-03 21:31:18 -0300

    visual

---
+++ android/app/src/main/kotlin/com/marbarret/calculator_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.marbarret.calculator_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/calculator_screen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String userInput = "";
+  String result = "0";
+
+  List<String> buttonList = [
+    'AC',
+    '(',
+    ')',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    'C',
+    '0',
+    '.',
+    '=',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1d2630),
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 3,
+            child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  userInput,
+                  style: const TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  result,
+                  style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          const Divider(color: Colors.white),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return CustomButton(buttonList[index]);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget CustomButton(String text) {
+    return InkWell(
+      splashColor: const Color(0xFF1d2630),
+      onTap: () {
+        setState(() {
+          handleButton(text);
+        });
+      },
+      child: Ink(
+        decoration: BoxDecoration(
+          color: getBGColor(text),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 2,
+              spreadRadius: 0.5,
+            )
+          ],
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                color: getColor(text),
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+
+  getColor(String text) {
+    if (text == "/" ||
+        text == "*" ||
+        text == "+" ||
+        text == "-" ||
+        text == "(" ||
+        text == ")" ||
+        text == "C") {
+      return const Color.fromARGB(255, 252, 100, 100);
+    }
+    return Colors.white;
+  }
+
+  getBGColor(String text) {
+    if (text == "AC") {
+      return const Color.fromARGB(255, 252, 100, 100);
+    }
+
+    if (text == "=") {
+      return const Color.fromARGB(255, 104, 204, 159);
+    }
+    return const Color(0xFF1d2630);
+  }
+
+  handleButton(String text) {
+    if(text == "AC") {
+      userInput = "";
+      result = "0";
+      return;
+    }
+
+    if(text == "C") {
+      if(userInput.isNotEmpty) {
+        userInput = userInput.substring(0, userInput.length -1);
+        return;
+      }
+      else {
+        return null;
+      }
+    }
+
+    userInput = userInput + text;
+  }
+}
